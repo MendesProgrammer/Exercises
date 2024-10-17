31637197f91732e1196e9c77d8af0634446e5e9f
Caio Mendes <160408013+MendesProgrammer@users.noreply.github.com>
2024-10-17 15:34:08 -0300

    Create frases.py

---
+++ Second_list/frases.py
@@ -0,0 +1,42 @@
+# Algoritmo que armazena frases do usuário, e caso queira, um sorteio destas frases e realizado
+import json
+from time import sleep
+from random import choice
+
+def digito(msg='', tempo=0.05):
+    for d in msg:
+        print(f'{d}', flush=True, end='')
+        sleep(tempo)
+    print('')
+
+
+try:
+    with open('arquivo.json', 'r') as file:
+        dado = json.loads(file.read())
+except:
+    dado = []
+    with open('arquivo.json', 'w') as file:
+        file.write(json.dumps(dado))
+
+sel = ''
+while sel != 'n':
+    print('-' * 50)
+    sel = str(input('Deseja adicionar uma frase ou sortear? (a, s, n) ')).strip().lower()
+    print('-' * 50)
+
+    if sel == 'a':
+        frase = str(input('Digite a frase: ')).strip()
+        dado.append(frase)
+
+    elif sel == 's':
+        item = choice(dado)
+        digito('Frase do dia...')
+        sleep(2)
+        digito(item)
+        sleep(2)
+
+    elif sel == 'n':
+        digito('Finalizando!')
+
+    else:
+        digito('Opção inválida!')
